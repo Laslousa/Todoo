8ccfe8fe9edb2dd9d91896f8d2528b121b2ba613
unknown <elyesbelaid.2005@gmail.com>
2025-08-30 10:30:32 +0200

    Finally!

--- index.js
+++ index.js
@@ -19,13 +19,28 @@ db.connect()
   .then(() => console.log("Connected to PostgreSQL database"))
   .catch((err) => console.error("Database connection error:", err));
 
+async function initializeDatabase() {
+  try {
+    if (users.length > 0) {
+      const usersResult = await db.query("SELECT * FROM users");
+      users = usersResult.rows;
+      return users[0];
+    } else {
+      return { user_id: 1, name: "Default", color: "Default" };
+    }
+  } catch (error) {
+    console.error("Error initializing database:", error);
+  }
+}
+
 let items = [];
 let users = [];
-let currentUser = { user_id: 1, name: "Default", color: "Default" };
+let currentUser = await initializeDatabase();
 let addOk = "New Item";
 let currentDate = new Date().toLocaleDateString("en-US", {
   weekday: "long",
 });
+
 app.get("/", async (req, res) => {
   let firstLoad = false;
   try {
@@ -36,13 +51,13 @@ app.get("/", async (req, res) => {
     }
 
     const usersResult = await db.query("SELECT * from users ORDER BY user_id");
+    users = usersResult.rows;
+
     const itemsResult = await db.query(
       "SELECT item_id, title FROM items WHERE user_id = $1 ORDER BY item_id",
       [currentUser.user_id]
     );
-
     items = itemsResult.rows;
-    users = usersResult.rows;
 
     // Récupérer les informations de l'utilisateur actuel
     if (users.length > 0) {
@@ -180,16 +195,17 @@ app.post("/changeUser", async (req, res) => {
       res.redirect("/");
     }
   } else if (mode === "edit") {
-    
-
-
-
-
-
-
-
-
-
+    try {
+      const userResult = await db.query(
+        "SELECT * FROM users WHERE user_id = $1",
+        [userId]
+      );
+      const userToEdit = userResult.rows[0];
+      res.render("newUser.ejs", { userToEdit: userToEdit });
+    } catch (error) {
+      console.error("Error fetching user for edit:", error);
+      res.redirect("/");
+    }
   }
 });
 
@@ -198,27 +214,95 @@ app.post("/addUser", async (req, res) => {
 });
 
 app.post("/createUser", async (req, res) => {
-  const newUserName = req.body.username.trim();
-  const selectedColor = req.body.color;
+  let newUserName = req.body.username.trim();
+  let selectedColor = req.body.color;
 
-  if (!newUserName) {
-    return res.redirect("/addUser");
-  }
-
-  try {
-    const result = await db.query(
-      "INSERT INTO users (name, color) VALUES ($1, $2) RETURNING *",
-      [newUserName, selectedColor]
-    );
-
-    // Changer vers le nouvel utilisateur créé
-    if (result.rows.length > 0) {
-      currentUser = result.rows[0];
+  if (req.body.action === "create") {
+    if (!newUserName) {
+      return res.redirect("/addUser");
     }
 
-    res.redirect("/");
-  } catch (error) {
-    console.error("Error creating user:", error);
-    res.redirect("/");
+    try {
+      const countResult = await db.query(
+        "SELECT COUNT(*) FROM users WHERE name = $1",
+        [newUserName]
+      );
+
+      if (parseInt(countResult.rows[0].count) > 0) {
+        // User already exists
+        return res.render("newUser.ejs", {
+          userAlreadyExists: "Username already exists, please choose another.",
+        });
+      }
+      if (!selectedColor) {
+        return res.render("newUser.ejs", {
+          userAlreadyExists: "Please select a color.",
+        });
+      }
+      const result = await db.query(
+        "INSERT INTO users (name, color) VALUES ($1, $2) RETURNING *",
+        [newUserName, selectedColor]
+      );
+
+      // Changer vers le nouvel utilisateur créé
+      if (result.rows.length > 0) {
+        currentUser = result.rows[0];
+      }
+
+      res.redirect("/");
+    } catch (error) {
+      console.error("Error creating user:", error);
+      res.redirect("/");
+    }
+  } else if (req.body.action === "update") {
+    const userId = req.body.userId;
+    if (!newUserName || isNaN(userId)) {
+      return res.redirect("/");
+    }
+
+    try {
+      const result = await db.query("SELECT * FROM users WHERE user_id = $1", [
+        userId,
+      ]);
+      const oldUser = result.rows[0];
+      const oldUserName = oldUser ? oldUser.name : "Unknown";
+      const oldColor = oldUser ? oldUser.color : "Unknown";
+      newUserName = newUserName || oldUserName;
+      selectedColor = selectedColor || oldColor;
+
+      try {
+        const otherUsers = await db.query(
+          "SELECT * FROM users WHERE user_id != $1",
+          [userId]
+        );
+        for (let otherUser of otherUsers.rows) {
+          if (otherUser.name === newUserName) {
+            console.error("Username already exists:", newUserName);
+            return res.render("newUser.ejs", {
+              userToEdit: oldUser,
+              userAlreadyExists:
+                "Username already exists, please choose another.",
+            });
+          }
+        }
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
+
+      await db.query(
+        "UPDATE users SET name = $1, color = $2 WHERE user_id = $3",
+        [newUserName, selectedColor, userId]
+      );
+
+      currentUser = {
+        user_id: parseInt(userId),
+        name: newUserName,
+        color: selectedColor,
+      };
+      res.redirect("/");
+    } catch (error) {
+      console.error("Error updating user:", error);
+      res.redirect("/");
+    }
   }
 });
