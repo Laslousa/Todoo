24c82e387f0c76220921dfce49f2fe8edc5c96ec
unknown <elyesbelaid.2005@gmail.com>
2025-08-30 15:39:17 +0200

    Convert DB_PORT to a number

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const db = isProd
       host: process.env.DB_HOST,
       database: process.env.DB_NAME,
       password: process.env.DB_PASSWORD,
-      port: process.env.DB_PORT,
+      port: Number(process.env.DB_PORT),
     });
 
 async function initializeDatabase() {
