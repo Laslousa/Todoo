f4216d51207f91d79d30a54b987d7a531b308299
unknown <elyesbelaid.2005@gmail.com>
2025-08-30 11:30:55 +0200

    Improve Security

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 import express from "express";
 import pg from "pg";
+import dotenv from "dotenv";
+
+// Charger les variables d'environnement
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.SERVER_PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "permalist",
-  password: "Laslousa+2005",
-  port: 5433,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 db.connect()
